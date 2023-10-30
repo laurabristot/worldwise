af378ca37c4d42e9b8fce115dff21f6b29e8218c
laura bristot <laurabristotbarbi@hotmail.com>
2023-10-30 15:35:30 -0300

    02 - implementing main pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Homepage, PageNotFound, Pricing, Product } from './pages'
+
 export default function App() {
-  return <div>WorldWise</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Homepage } from './Homepage.jsx'
+export { default as PageNotFound } from './PageNotFound.jsx'
+export { default as Pricing } from './Pricing.jsx'
+export { default as Product } from './Product.jsx'
