e4ff35e8eae520ce9bb385677cadc8d277bae481
laura bristot <laurabristotbarbi@hotmail.com>
2023-10-30 19:13:58 -0300

    13 -  Programmatic Navigation with <Navigate />

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { City, CityList, CountryList, Form } from './components'
 import {
   AppLayout,
@@ -39,10 +39,7 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
