333ecbcd46885ada393dd2b3f50d3c5174edd172
laura bristot <laurabristotbarbi@hotmail.com>
2023-10-31 16:47:07 -0300

    19 - Interacting With the Map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,27 +1,35 @@
-import { useState } from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react'
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvent
+} from 'react-leaflet'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { useCities } from '../../contexts/CitiesContext'
 import styles from './Map.module.css'
 
 export default function Map() {
   const { cities } = useCities()
-  const navigate = useNavigate()
   const [mapPosition, setMapPosition] = useState([40, 0])
   const [searchParams, setSearchParams] = useSearchParams()
   const lat = searchParams.get('lat')
   const lng = searchParams.get('lng')
 
+  useEffect(() => {
+    if (lat && lng) {
+      setMapPosition([lat, lng])
+    }
+  }, [lat, lng])
+
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate('form')
-      }}
-    >
+    <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -42,7 +50,25 @@ export default function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   )
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap()
+  map.setView(position)
+  return null
+}
+
+function DetectClick() {
+  const navigate = useNavigate()
+
+  useMapEvent({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+    }
+  })
 }
